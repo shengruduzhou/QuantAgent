f859e52b2be44787c921462a885aee1f258c60b0
渡 <167118127+shengruduzhou@users.noreply.github.com>
2026-07-13 03:14:19 +0800

    PR-8: export factor governance metrics

--- src/quantagent/factors/__init__.py
+++ src/quantagent/factors/__init__.py
@@ -1,4 +1,10 @@
 from quantagent.factors.registry import FactorMeta, FactorOutput, FactorRegistry, default_registry
+from quantagent.factors.governance_metrics import (
+    FactorGateConfig,
+    FactorGovernanceReport,
+    correlation_clusters,
+    evaluate_factor_candidate,
+)
 
 try:
     from quantagent.factors import alpha101 as alpha101
@@ -14,13 +20,17 @@ except Exception:
     technical_indicators = None
 
 __all__ = [
+    "FactorGateConfig",
+    "FactorGovernanceReport",
     "FactorMeta",
     "FactorOutput",
     "FactorRegistry",
-    "default_registry",
     "alpha101",
     "alpha181",
     "cicc_ashare80",
     "cicc_high_freq",
+    "correlation_clusters",
+    "default_registry",
+    "evaluate_factor_candidate",
     "technical_indicators",
 ]
