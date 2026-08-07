5970ccd55981ee32ee0d39d86b89ec07e90cd389
渡 <167118127+shengruduzhou@users.noreply.github.com>
2026-08-07 15:29:26 +0900

    feat(data): export hithink finance provider

--- src/quantagent/data/providers/__init__.py
+++ src/quantagent/data/providers/__init__.py
@@ -1,6 +1,7 @@
 from quantagent.data.providers.base import ProviderRequest, ProviderResult
 from quantagent.data.providers.akshare_live_provider import AkShareLiveProvider
 from quantagent.data.providers.disclosure_provider import DisclosureWebProvider
+from quantagent.data.providers.hithink_finance_provider import FuyaoProvider, HithinkFinanceProvider
 from quantagent.data.providers.local_csv_provider import LocalCsvProvider
 from quantagent.data.providers.mock_provider import MockProvider
 from quantagent.data.providers.news_provider import NewsWebProvider
@@ -13,6 +14,8 @@ from quantagent.data.providers.v7_research_provider import LocalV7ResearchProvid
 __all__ = [
     "AkShareLiveProvider",
     "DisclosureWebProvider",
+    "FuyaoProvider",
+    "HithinkFinanceProvider",
     "ProviderRequest",
     "ProviderResult",
     "MockProvider",
